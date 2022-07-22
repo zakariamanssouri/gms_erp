6ab0fa851b0d00959ca6280ec15071641f0febaa
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-07-22 13:48:48 +0100

    add config folder for global variables

---
+++ lib/config/global_params.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class GlobalParams {
+// api
+  static final baseUrl = 'http://144.91.76.98:84/AndroidDB/';
+// for screens
+  static final MainColor = Color(0xFFCDDEEC);
+  static final SecondaryColor = Color(0xFF3E30BC);
+  static final backgroundColor = Color(0xffEDEDED);
+  static final double MainPadding = 20;
+
+  static final List<Map<String, dynamic>> menus = [
+    {
+      "title": "Inventory",
+      "icon": Icon(
+        Icons.dashboard,
+        color: Colors.blue,
+      ),
+      "route": "/inventory"
+    },
+    {
+      "title": "test",
+      "icon": Icon(
+        Icons.telegram,
+        color: Colors.blue,
+      ),
+      "route": "/test"
+    },
+  ];
+
+  // inventory details card header gradient colors
+  static final List<Color> inventoryDetailsCardHeaderGradientColors = [
+    Colors.blue,
+    Colors.blue,
+  ];
+}
