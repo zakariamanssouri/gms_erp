f3cf95edab8e653e079bf62f52ecea65ef0acc95
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-07-23 13:39:56 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gms_erp/blocs/Inventory/bloc/inventory_bloc.dart';
@@ -22,30 +21,3 @@ void main() {
 
 
 
-
-
-// class MyApp2 extends StatelessWidget {
-//   const MyApp2({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiBlocProvider(
-//       providers: [
-//         BlocProvider<InventoryBloc>(
-//           create: (context) => InventoryBloc()..add(LoadInventories()),
-//         ),
-//         BlocProvider<ProductDetailsBloc>(
-//             create: (context) => ProductDetailsBloc()),
-//       ],
-//       child: MaterialApp(
-//         routes: {
-//           '/inventory': (context) => InventoryPage(),
-//           '/inventory_details': (context) => InventoyDetailsPage(),
-//         },
-//         debugShowCheckedModeBanner: false,
-//         title: 'Flutter Demo',
-//         home: InventoryPage(),
-//       ),
-//     );
-//   }
-// }
