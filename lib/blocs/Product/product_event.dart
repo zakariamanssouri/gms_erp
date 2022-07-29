f28b06b2c8615b998ccd180db09f49ce51e444b6
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-07-29 17:40:21 +0100

    add update product event

--- lib/blocs/Product/product_event.dart
+++ lib/blocs/Product/product_event.dart
@@ -2,7 +2,7 @@ part of 'product_bloc.dart';
 
 abstract class ProductEvent extends Equatable {
   const ProductEvent();
-
+  
   @override
   List<Object> get props => [];
 }
@@ -15,8 +15,17 @@ class SearchProductEvent extends ProductEvent {
   String search_value;
   List<Product> product_list = [];
 
-  SearchProductEvent({required this.search_value,required  this.product_list});
+  SearchProductEvent({required this.search_value, required this.product_list});
 
   @override
   List<Object> get props => [search_value, product_list];
+}
+
+class UpdateProductEvent extends ProductEvent {
+  Product product;
+
+  UpdateProductEvent({required this.product});
+
+  @override
+  List<Object> get props => [product];
 }
