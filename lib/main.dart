c98e5ce790222e916a269ee98c0ca773cd048163
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-07-30 14:43:07 +0100

    remove multiblocprovider from main

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gms_erp/blocs/Inventory/bloc/inventory_bloc.dart';
-import 'package:gms_erp/blocs/Client/client_bloc.dart';
-import 'package:gms_erp/blocs/InventoryDetails/inventory_details_bloc.dart';
-import 'package:gms_erp/homepage.dart';
 import 'identity/views/Sign in/userlogin.views.dart';
-import 'identity/views/user.view.dart';
-import 'identity/views/useradd.view.dart';
 
 
 void main() {
-  runApp(MultiBlocProvider(providers: [
-     BlocProvider<InventoryBloc>(
-          create: (context) => InventoryBloc(),
-        ),
-      
-  ],child: MaterialApp(
-     home: UserLogin())));
+  runApp(
+    MaterialApp(
+     home: UserLogin()));
 }
 
 
