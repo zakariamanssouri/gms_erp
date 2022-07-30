1037cc564069c3b04d6c56920f6c54e66dcc0cb0
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-07-30 14:25:32 +0100

    change blocbuilder to bloc listener ( we use it when we want to show a snackbar or an Alert ...)

--- lib/inventory/views/Products/Details/product_details.dart
+++ lib/inventory/views/Products/Details/product_details.dart
@@ -4,7 +4,6 @@ import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gms_erp/blocs/Client/client_bloc.dart';
 import 'package:gms_erp/blocs/Product/product_bloc.dart';
 import 'package:gms_erp/config/global_params.dart';
 import 'package:gms_erp/crm/models/Product.dart';
@@ -30,142 +29,168 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+
     return Scaffold(
         appBar: AppBar(
           elevation: 0,
           title: Text('Product Details'),
         ),
-        body: ListView(children: <Widget>[
-          Card(
-              child: ListTile(
-            minLeadingWidth: 70,
-            leading: const Text('Numéro',
-                style: TextStyle(
-                    fontFamily: GlobalParams.MainfontFamily,
-                    fontWeight: FontWeight.w900)),
-            title: Text(
-              widget.product.no,
-              style: const TextStyle(
-                  fontFamily: GlobalParams.MainfontFamily,
-                  fontWeight: FontWeight.w300),
-            ),
-          )),
-          Card(
-              child: ListTile(
-            minLeadingWidth: 70,
-            leading: const Text('Noms',
-                style: TextStyle(
-                    fontFamily: GlobalParams.MainfontFamily,
-                    fontWeight: FontWeight.w900)),
-            title: Text(widget.product.name,
-                style: const TextStyle(
-                  fontFamily: GlobalParams.MainfontFamily,
-                  fontWeight: FontWeight.w300,
-                )),
-          )),
-          Card(
-              child: ListTile(
-            minLeadingWidth: 70,
-            leading: Text('Code Bar',
-                style: const TextStyle(
-                    fontFamily: GlobalParams.MainfontFamily,
-                    fontWeight: FontWeight.w900)),
-            title: Text(barcode,
-                style: TextStyle(
-                  fontFamily: GlobalParams.MainfontFamily,
-                  fontWeight: FontWeight.w300,
-                )),
-            trailing: FloatingActionButton(
-                disabledElevation: 0,
-                mini: true,
-                backgroundColor: Colors.deepOrange,
-                child: Icon(
-                  Icons.qr_code_scanner_rounded,
-                  size: 20,
+        body: BlocListener<ProductBloc, ProductState>(
+            listener: (context, state) {
+              if (state.requestState == ProductRequestState.Updating) {
+              } else if (state.requestState == ProductRequestState.Updated) {
+                CoolAlert.show(
+                  context: context,
+                  type: CoolAlertType.success,
+                  text: 'le produit a été mis à jour avec succès',
+                );
+                BlocProvider.of<ProductBloc>(context)
+                    .add(LoadAllProductsEvent());
+                Navigator.pop(context);
+                // BlocProvider.of<ProductBloc>(context)
+                //     .add(LoadAllProductsEvent());
+                // Navigator.pop(context); // error on updating
+              } else if (state.requestState == ProductRequestState.Error) {
+                CoolAlert.show(
+                  context: context,
+                  type: CoolAlertType.error,
+                  text: 'Erreur lors de la mise à jour du produit',
+                );
+              }
+            },
+            child: ListView(children: <Widget>[
+              Card(
+                  child: ListTile(
+                minLeadingWidth: 70,
+                leading: const Text('Numéro',
+                    style: TextStyle(
+                        fontFamily: GlobalParams.MainfontFamily,
+                        fontWeight: FontWeight.w900)),
+                title: Text(
+                  widget.product.no,
+                  style: const TextStyle(
+                      fontFamily: GlobalParams.MainfontFamily,
+                      fontWeight: FontWeight.w300),
                 ),
-                onPressed: () async {
-                  String barcodeScanRes =
-                      await FlutterBarcodeScanner.scanBarcode(
-                          "blue", "cancel", true, ScanMode.BARCODE);
-                  setState(() {
-                    barcode = barcodeScanRes;
-                    widget.product.code = barcode;
-                  });
-                }),
-          )),
-          Card(
-              child: ListTile(
-            minLeadingWidth: 70,
-            leading: Text('Stock Min',
-                style: const TextStyle(
-                    fontFamily: GlobalParams.MainfontFamily,
-                    fontWeight: FontWeight.w900)),
-            title: Text(widget.product.stock_min,
-                style: TextStyle(
-                  fontFamily: GlobalParams.MainfontFamily,
-                  fontWeight: FontWeight.w300,
-                )),
-          )),
-          Card(
-              child: ListTile(
-            minLeadingWidth: 70,
-            leading: const Text('Prix',
-                style: const TextStyle(
-                    fontFamily: GlobalParams.MainfontFamily,
-                    fontWeight: FontWeight.w900)),
-            title: Text(widget.product.s_price,
-                style: TextStyle(
-                  fontFamily: GlobalParams.MainfontFamily,
-                  fontWeight: FontWeight.w300,
-                )),
-          )),
-          Card(
-              child: ListTile(
-            minLeadingWidth: 70,
-            leading: Text('Prix Min',
-                style: const TextStyle(
-                    fontFamily: GlobalParams.MainfontFamily,
-                    fontWeight: FontWeight.w900)),
-            title: Text(widget.product.s_price_min,
-                style: TextStyle(
-                  fontFamily: GlobalParams.MainfontFamily,
-                  fontWeight: FontWeight.w300,
-                )),
-          )),
-          Container(
-              margin: EdgeInsets.only(
-                  top: GlobalParams.MainPadding / 2,
-                  right: GlobalParams.MainPadding / 2,
-                  left: GlobalParams.MainPadding / 2),
-              child: ButtonWidget(
-                  size: size,
-                  onPressed: () async {
-                    BlocProvider.of<ProductBloc>(context).add(
-                      UpdateProductEvent(product: widget.product),
-                    );
+              )),
+              Card(
+                  child: ListTile(
+                minLeadingWidth: 70,
+                leading: const Text('Noms',
+                    style: TextStyle(
+                        fontFamily: GlobalParams.MainfontFamily,
+                        fontWeight: FontWeight.w900)),
+                title: Text(widget.product.name,
+                    style: const TextStyle(
+                      fontFamily: GlobalParams.MainfontFamily,
+                      fontWeight: FontWeight.w300,
+                    )),
+              )),
+              Card(
+                  child: ListTile(
+                minLeadingWidth: 70,
+                leading: Text('Code Bar',
+                    style: const TextStyle(
+                        fontFamily: GlobalParams.MainfontFamily,
+                        fontWeight: FontWeight.w900)),
+                title: Text(barcode,
+                    style: TextStyle(
+                      fontFamily: GlobalParams.MainfontFamily,
+                      fontWeight: FontWeight.w300,
+                    )),
+                trailing: FloatingActionButton(
+                    heroTag: 'barcode-button',
+                    disabledElevation: 0,
+                    mini: true,
+                    backgroundColor: Colors.deepOrange,
+                    child: Icon(
+                      Icons.qr_code_scanner_rounded,
+                      size: 20,
+                    ),
+                    onPressed: () async {
+                      String barcodeScanRes =
+                          await FlutterBarcodeScanner.scanBarcode(
+                              "blue", "cancel", true, ScanMode.BARCODE);
+                      setState(() {
+                        barcode = barcodeScanRes;
+                        widget.product.code = barcode;
+                      });
+                    }),
+              )),
+              Card(
+                  child: ListTile(
+                minLeadingWidth: 70,
+                leading: Text('Stock Min',
+                    style: const TextStyle(
+                        fontFamily: GlobalParams.MainfontFamily,
+                        fontWeight: FontWeight.w900)),
+                title: Text(widget.product.stock_min,
+                    style: TextStyle(
+                      fontFamily: GlobalParams.MainfontFamily,
+                      fontWeight: FontWeight.w300,
+                    )),
+              )),
+              Card(
+                  child: ListTile(
+                minLeadingWidth: 70,
+                leading: const Text('Prix',
+                    style: const TextStyle(
+                        fontFamily: GlobalParams.MainfontFamily,
+                        fontWeight: FontWeight.w900)),
+                title: Text(widget.product.s_price,
+                    style: TextStyle(
+                      fontFamily: GlobalParams.MainfontFamily,
+                      fontWeight: FontWeight.w300,
+                    )),
+              )),
+              Card(
+                  child: ListTile(
+                minLeadingWidth: 70,
+                leading: Text('Prix Min',
+                    style: const TextStyle(
+                        fontFamily: GlobalParams.MainfontFamily,
+                        fontWeight: FontWeight.w900)),
+                title: Text(widget.product.s_price_min,
+                    style: TextStyle(
+                      fontFamily: GlobalParams.MainfontFamily,
+                      fontWeight: FontWeight.w300,
+                    )),
+              )),
+              Container(
+                  margin: EdgeInsets.only(
+                      top: GlobalParams.MainPadding / 2,
+                      right: GlobalParams.MainPadding / 2,
+                      left: GlobalParams.MainPadding / 2),
+                  child: ButtonWidget(
+                      size: size,
+                      onPressed: () async {
+                        BlocProvider.of<ProductBloc>(context)
+                          ..add(
+                            UpdateProductEvent(product: widget.product),
+                          );
 
-                    print(BlocProvider.of<ProductBloc>(context)
-                        .state
-                        .requestState);
-                    if (BlocProvider.of<ProductBloc>(context)
-                            .state
-                            .requestState ==
-                        RequestState.Error) {
-                      print("hit here");
-                    }
+                        // print(BlocProvider.of<ProductBloc>(context)
+                        //     .state
+                        //     .requestState);
+                        // if (BlocProvider.of<ProductBloc>(context)
+                        //         .state
+                        //         .requestState ==
+                        //     RequestState.Error) {
+                        //   print("hit here");
+                        // }
 
-                    if (BlocProvider.of<ProductBloc>(context)
-                            .state
-                            .requestState !=
-                        ProductRequestState.Error) {
-                      CoolAlert.show(
-                          context: context,
-                          type: CoolAlertType.success,
-                          text: "le produit a été mis à jour avec succès");
-                      Navigator.pop(context);
-                    }
-                  },
-                  text: "Modifier"))
-        ]));
+                        // if (BlocProvider.of<ProductBloc>(context)
+                        //         .state
+                        //         .requestState !=
+                        //     ProductRequestState.Error) {
+                        //   CoolAlert.show(
+                        //       context: context,
+                        //       type: CoolAlertType.success,
+                        //       text: "le produit a été mis à jour avec succès");
+                        //   Navigator.pop(context);
+                        // }
+                      },
+                      text: "Modifier"))
+            ])));
   }
 }
