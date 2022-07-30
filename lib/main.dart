b24926f4344bf501f19950ed593fe403ed99e944
b.mohamed <b.mohamed@devaxy.com>
2022-07-30 15:47:05 +0100

    resolve main conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gms_erp/blocs/Inventory/bloc/inventory_bloc.dart';
-import 'package:gms_erp/blocs/Client/client_bloc.dart';
-import 'package:gms_erp/blocs/InventoryDetails/inventory_details_bloc.dart';
-import 'package:gms_erp/homepage.dart';
 import 'identity/views/Sign in/user_login.view.dart';
-import 'identity/views/user.view.dart';
-import 'identity/views/Sign in/user.signup.view.dart';
-
 
 void main() {
-  runApp(MultiBlocProvider(providers: [
-     BlocProvider<InventoryBloc>(
-          create: (context) => InventoryBloc(),
-        ),
-      
-  ],child: MaterialApp(
-     home: UserLogin())));
+  runApp(
+      MaterialApp(
+          home: UserLogin()));
 }
 
 
