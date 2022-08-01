0aea6ea1c5b16cc07a876384b4cf143e573e99dd
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-08-01 15:17:06 +0100

    update products page

--- lib/inventory/views/Products/products.dart
+++ lib/inventory/views/Products/products.dart
@@ -1,27 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gms_erp/blocs/Product/product_bloc.dart';
 import 'package:gms_erp/config/global_params.dart';
 import 'package:gms_erp/inventory/views/Products/widgets/ProductsPageBody.dart';
-
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    BuildContext _context = context;
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      floatingActionButton: FloatingActionButton(
+          disabledElevation: 0,
+          backgroundColor: Colors.purple[900],
+          child: Icon(
+            Icons.qr_code_scanner_rounded,
+            size: 30,
+          ),
+          onPressed: () async {
+            print(BlocProvider.of<ProductBloc>(_context));
+            String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+                "blue", "cancel", false, ScanMode.BARCODE);
+            BlocProvider.of<ProductBloc>(context).add(SearchProductEvent(
+                search_value: barcodeScanRes,
+                product_list:
+                    BlocProvider.of<ProductBloc>(context).state.products));
+          }),
       backgroundColor: GlobalParams.backgroundColor,
       appBar: AppBar(
         title: const Text('Products'),
         elevation: 0,
         backgroundColor: Colors.blue,
       ),
-      body: BlocProvider(
-        create: (context) => ProductBloc()..add(const LoadAllProductsEvent()),
-        child: const ProductsPageBody(),
-      ),
+      body: const ProductsPageBody(),
     );
   }
 }
