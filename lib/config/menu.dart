a7997c00ed9d6dca9395a0dd3113cd45dc992498
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-08-03 15:28:06 +0100

    add button for purshased orders

--- lib/config/menu.dart
+++ lib/config/menu.dart
@@ -62,9 +62,13 @@ class ButtonOption {
         route: "/Product",
         text: 'Produits',
         iconData: Icons.production_quantity_limits),
-        const ButtonOption(
+    const ButtonOption(
         route: "/Activity",
         text: 'Vendeurs',
+        iconData: Icons.account_balance_wallet),
+    const ButtonOption(
+        route: "/PurchasedOrders",
+        text: 'Purchased Orders',
         iconData: Icons.account_balance_wallet),
   ];
 
