af6df6c515f1d76091fcde9f38dd80d54ad596ce
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-08-03 15:29:41 +0100

    update SearchField

--- lib/widgets/SearchField.dart
+++ lib/widgets/SearchField.dart
@@ -1,9 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../config/global_params.dart';
 
-class SearchField extends StatelessWidget {
+class SearchField extends StatefulWidget {
   final Size size;
   final void Function(String)? onchanged_function;
 
@@ -11,32 +13,47 @@ class SearchField extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
+  late TextEditingController controller;
+  late void Function(String)? onchanged_function;
+  @override
+  void initState() {
+    // TODO: implement initState
+    controller = TextEditingController();
+    onchanged_function = widget.onchanged_function;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      padding: EdgeInsets.symmetric(horizontal: GlobalParams.MainPadding),
+      width: double.infinity,
+      height: widget.size.height * 0.1,
       margin: EdgeInsets.symmetric(horizontal: GlobalParams.MainPadding),
-      height: size.height * 0.09,
+      padding: EdgeInsets.symmetric(horizontal: GlobalParams.MainPadding),
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-                color: Color.fromARGB(246, 190, 185, 185),
-                blurRadius: 25,
-                spreadRadius: 5,
-                offset: Offset(20, 20))
-          ]),
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
       child: TextField(
         onChanged: onchanged_function,
+        controller: controller,
         decoration: InputDecoration(
-            hintText: "Search",
-            hintStyle:
-                TextStyle(color: GlobalParams.SecondaryColor.withOpacity(0.5)),
-            enabledBorder: InputBorder.none,
-            focusedBorder: InputBorder.none,
-            suffixIcon: Icon(Icons.search),
-            suffixIconColor: Colors.blue),
+          hintText: "Search",
+          hintStyle: TextStyle(color: Colors.grey.withOpacity(0.5)),
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          suffixIconColor: Colors.blue,
+          suffixIcon: IconButton(
+            splashColor: Colors.transparent,
+            icon: Icon(Icons.clear),
+            onPressed: () {
+              setState(() {
+                controller.text = "";
+              });
+            },
+          ),
+        ),
       ),
     );
   }
