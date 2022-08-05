7f290c99d77f110b23aafec05be05044c71fe3b5
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-08-05 14:47:18 +0100

    update home button

--- lib/widgets/homebutton.dart
+++ lib/widgets/homebutton.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:gms_erp/config/menu.dart';
 import 'myalert.dart';
 
-
 class HomeButton extends StatelessWidget {
   const HomeButton({Key? key, required this.buttonOption}) : super(key: key);
   final ButtonOption buttonOption;
@@ -13,38 +12,38 @@ class HomeButton extends StatelessWidget {
   //context a ne jamais changer
   Widget build(BuildContext context) {
     final textStyle = Theme.of(context).textTheme.bodyText1;
+    Size size = MediaQuery.of(context).size;
 
-    return Card(
+    return Container(
+        width: size.width * 0.4,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
           color: Colors.blue,
-          child: InkWell(
-          onTap: () 
-          { 
-          Navigator.pushNamed(context, buttonOption.route);
-          },
-        child: Center(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                    child: Icon(buttonOption.iconData,
-                        size: 45.0, color: Colors.white)),
-                // ignore: unnecessary_new
-                new TextButton(
-                  child: Text(
-                    buttonOption.text,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () {
-                    print("clicked");
-                    Navigator.pushNamed(context, buttonOption.route);
-                  },
-                ),
-                 Padding(
-                 padding: const EdgeInsets.all(8),
-                  child: Text(''),
-                 ),
-              ]),
         ),
+        child: InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, buttonOption.route);
+          },
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(buttonOption.iconData,
+                      size: size.width * 0.10, color: Colors.white),
+                  // ignore: unnecessary_new
+                  new TextButton(
+                    child: Text(
+                      buttonOption.text,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      print("clicked");
+                      Navigator.pushNamed(context, buttonOption.route);
+                    },
+                  ),
+                ]),
+          ),
         ));
   }
 }
